19db11f2069f6177647580ede4f10acbf39b869c
plamendimitrov <pgdimitrov@yahoo.com>
2025-04-29 15:41:56 +0300

    bug

--- elo04/settings.py
+++ elo04/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-m76nt%l&)pl7i&(2*jj*g1x5pu(u6=n&^l%gl_$zx3ovb0b-j='
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Read DEBUG from environment variable (default: True for development)
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 
 
